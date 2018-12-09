36cc4fc989815bcdc9882d7b2860126f51eaab60
romqu <roman.quistler@gmail.com>
2018-12-09 10:02:44 +0100

    add '@Component' annotation to AppComponent and create it inside Application class

--- app/src/main/java/de/romqu/dagger_example/app/AppComponent.kt
+++ app/src/main/java/de/romqu/dagger_example/app/AppComponent.kt
@@ -3,5 +3,6 @@ package de.romqu.dagger_example.app
 import dagger.Component
 import javax.inject.Singleton
 
+@Component
 interface AppComponent {
 }
--- app/src/main/java/de/romqu/dagger_example/app/DaggerExampleApp.kt
+++ app/src/main/java/de/romqu/dagger_example/app/DaggerExampleApp.kt
@@ -2,11 +2,16 @@ package de.romqu.dagger_example.app
 
 import android.app.Application
 
-class DaggerExampleApp: Application() {
+class DaggerExampleApp : Application() {
+
+    val appComponent by lazy {
+
+        DaggerAppComponent
+            .create()
+    }
 
     override fun onCreate() {
         super.onCreate()
-
     }
 
 }
