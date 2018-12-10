92786acbcd2112ecaebda034957907bb3406118b
romqu <roman.quistler@gmail.com>
2018-12-10 11:34:23 +0100

    refactor

--- app/src/main/java/de/romqu/dagger_example/app/AppComponent.kt
+++ app/src/main/java/de/romqu/dagger_example/app/AppComponent.kt
@@ -1,14 +1,11 @@
 package de.romqu.dagger_example.app
 
 import dagger.Component
-import de.romqu.dagger_example.logic.AppLogic
 import javax.inject.Singleton
 
 @Component
 @Singleton
 interface AppComponent {
-
-    fun appLogic(): AppLogic
 
     fun inject(daggerExampleApp: DaggerExampleApp)
 }
