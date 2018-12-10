3f20deb72a6838cc2b92311eba48e0676cce8a56
romqu <roman.quistler@gmail.com>
2018-12-10 09:52:07 +0100

    add possible ways to retrieve an instance

--- app/src/main/java/de/romqu/dagger_example/app/AppComponent.kt
+++ app/src/main/java/de/romqu/dagger_example/app/AppComponent.kt
@@ -1,10 +1,13 @@
 package de.romqu.dagger_example.app
 
 import dagger.Component
+import de.romqu.dagger_example.logic.AppLogic
 import javax.inject.Singleton
 
 @Component
 interface AppComponent {
+
+    fun appLogic(): AppLogic
 
     fun inject(daggerExampleApp: DaggerExampleApp)
 }
--- app/src/main/java/de/romqu/dagger_example/app/DaggerExampleApp.kt
+++ app/src/main/java/de/romqu/dagger_example/app/DaggerExampleApp.kt
@@ -3,25 +3,41 @@ package de.romqu.dagger_example.app
 import android.app.Application
 import de.romqu.dagger_example.logic.AppLogic
 import javax.inject.Inject
+import javax.inject.Provider
 
 class DaggerExampleApp : Application() {
 
     @Inject
-    lateinit var appLogic: AppLogic
+    lateinit var appLogicInject: AppLogic
 
-    val appComponent by lazy {
+    @Inject
+    lateinit var provider: Provider<AppLogic>
+
+    private val appComponent by lazy {
 
         DaggerAppComponent
             .create()
     }
 
+    private val appLogicComponent by lazy {
+        appComponent.appLogic()
+    }
+
+    private val appLogicProvider by lazy {
+        provider.get()
+    }
 
     override fun onCreate() {
         super.onCreate()
 
         appComponent.inject(this)
 
-        appLogic.execute()
+        appLogicInject.execute()
+
+        appLogicComponent.execute()
+
+        appLogicProvider.execute()
+
     }
 
 }
